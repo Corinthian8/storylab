729e811734225bdb8f95c6f464f010c0fe93ec62
Pablo Bugueno <pablo.buguenoe@gmail.com>
2023-08-03 19:54:17 +0900

    added blueprint and script migrations

---
+++ db/migrate/20230803104622_create_blueprints.rb
@@ -0,0 +1,11 @@
+class CreateBlueprints < ActiveRecord::Migration[7.0]
+  def change
+    create_table :blueprints do |t|
+      t.string :name
+      t.text :prompt_template
+      t.text :sample_videos, array: true, default: []
+
+      t.timestamps
+    end
+  end
+end
