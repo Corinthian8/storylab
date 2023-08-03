e7a699932452682f61d6055b685f9daa8db01bb3
光川聖恵 <kiyo0708mk@gmail.com>
2023-08-03 20:51:34 +0900

    added show for scripts

--- app/controllers/scripts_controller.rb
+++ app/controllers/scripts_controller.rb
@@ -1,18 +1,7 @@
 class ScriptsController < ApplicationController
-  before_action :set_script, only: [:show]
 
-  # def index
-  #   @scripts = Script.all
-  # end
 
   def show
-    @blueprint = Blueprint.new
-    @blueprints = @script.blueprints
-  end
-
-  private
-
-  def set_script
     @script = Script.find(params[:id])
   end
 
