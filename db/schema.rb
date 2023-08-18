9d9df5b3d0e05bdafdb777c36d78ce089082c42a
Kyle Nichols <kyle.r.nichols@gmail.com>
2023-08-19 02:25:21 +0900

    word cloud no longer hard coded

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_17_113758) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_18_153823) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,6 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_17_113758) do
     t.text "sample_videos", default: [], array: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "word_cloud", array: true
   end
 
   create_table "chats", force: :cascade do |t|
