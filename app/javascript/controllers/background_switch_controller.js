bdf0452a23534635a90e739fdc7c4f2d745d7438
Kyle Nichols <kyle.r.nichols@gmail.com>
2023-08-22 21:30:18 +0900

    background images randomly light up

--- app/javascript/controllers/background_switch_controller.js
+++ app/javascript/controllers/background_switch_controller.js
@@ -3,20 +3,24 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="background-switch"
 export default class extends Controller {
   static targets = ["block1", "block2", "block3", "block4", "block5", "block6",
-                    "block7", "block8", "block9", "block10", "block11", "block12", "main-page" ]
-  connect() {
-    console.log("hey from the switch")
-  }
+                    "block7", "block8", "block9", "block10", "block11", "block12" ]
 
-
-  toggle() {
-    var block = getElementbyId(`block${Math.ceil(Math.random() * 12)}`);
-    block.classList.remove("blacked-out");
-    setTimeout(() => {
-      block.classList.add("blacked-out");
-    }, 2000)
-    // this.block1Target.classList.remove("blacked-out");
-    // this.block1Target.classList.add("blacked-out");
+    connect() {
+    const imageDivs = [this.block1Target, this.block2Target, this.block3Target, this.block4Target, this.block5Target, this.block6Target,
+              this.block7Target, this.block8Target, this.block9Target, this.block10Target, this.block11Target, this.block12Target, ]
+    let index
+    setInterval(() => {
+      imageDivs.forEach( (div) => {
+        div?.classList.remove("show-image");
+      })
+      let randomIndex = Math.ceil(Math.random() * imageDivs.length)
+      while (index == randomIndex) {
+        randomIndex = Math.ceil(Math.random() * imageDivs.length)
+      }
+      index = randomIndex
+      console.log(randomIndex);
+      imageDivs[randomIndex]?.classList.add("show-image");
+    }, 2000);
   }
 
 }
