9152436e9e15753dc1a85b0daba5147dd30dea22
光川聖恵 <kiyo0708mk@gmail.com>
2023-08-24 22:07:49 +0900

    added location to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_22_123650) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_24_125500) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,6 +57,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_22_123650) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_chats_on_user_id"
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "messages", force: :cascade do |t|
