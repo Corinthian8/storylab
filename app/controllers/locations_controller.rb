fc66423b37cadcc0e4671db2620cb3cbe94a7639
光川聖恵 <kiyo0708mk@gmail.com>
2023-08-26 15:33:42 +0900

    finished location suggestion

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -17,12 +17,27 @@ class LocationsController < ApplicationController
   end
 
   def create
+    @location = Location.new(location_params)
+    @location.script_id = params[:script_id]
+    # @script.user = current_user
+    @location.content = ''
+    if @location.content.save
+      redirect_to location_path(params[:script_id], @location)
+    else
+      render 'locations/show'
+    end
   end
 
+  # PATCH/PUT /scripts/:script_id/locations/:id
   def update
   end
 
   private
+
+  def location_params
+    params.require(:location).permit(:content)
+  end
+
 
   def set_location
     @location = Location.find(params[:id])
