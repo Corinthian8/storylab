508a3de9f8b6b3d23fb24d04b487335aabee239b
Pablo Bugueno <pablo.buguenoe@gmail.com>
2023-08-29 21:30:23 +0900

    tuned prompts

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -4,17 +4,19 @@ class LocationsController < ApplicationController
   def show
     @pexels_videos = pexels(@location.script.topic)
     @plan = @location.script.plan
-    fill_plan(@plan)
     if @location.content.blank?
       response = ChatgptService.call("Give me a list of 5 non-specific locations (for example: 'Park' instead 'Central Park, New York')  to film a video that is based on following script:
-        #{@location.script.script_body}
-        Just give an enumerated list of locations, without a prefacing paragraph, specify what should be the content to record.")
+      #{@location.script.script_body}
+      Just give an enumerated list of locations, without a prefacing paragraph, specify what should be the content to record.
+      Write it as HTML, using only h4, h5, and p tags in HTML code but don't include the head tag.")
       @location.update(content: response)
+      fill_plan(@plan)
       render :show
     else
       respond_to do |format|
+        fill_plan(@plan)
         format.html
-        format.text { render :show, locals: { location: @location }, formats: [:html] }
+        format.text { render :show, locals: { location: @location, plan: @plan }, formats: [:html] }
       end
     end
   end
@@ -43,15 +45,16 @@ class LocationsController < ApplicationController
     if plan.content.blank?
       response = ChatgptService.call("Give me a step by step work plan to record and edit the youtube video based on following script:
         '#{@plan.script.script_body}'
-        The plan should consider the recording order of event to make the video easier to record and edit.
-        Just give the working plan without a prefacing paragraph.")
+        The plan's goal is to make the video easier to record and edit.
+        I want to record all of the scenes that take place in sequence, rather than recording in the order of the script.
+        Just give the working plan without a prefacing paragraph. Write it as HTML, using only h4, h5, and p tags in HTML code but don't include the head tag.")
       plan.update(content: response)
       render :show
-    else
-      respond_to do |format|
-        format.html
-        format.text { render :show, locals: { plan: plan }, formats: [:html] }
-      end
+    # else
+    #   respond_to do |format|
+    #     format.html
+    #     format.text { render :show, locals: { plan: plan }, formats: [:html] }
+    #   end
     end
   end
 end
